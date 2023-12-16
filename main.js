83117ef09b3d34832b238a4aa1ed8df0388d36e7
Branislav Provonov <121875013+GitomaniaFan@users.noreply.github.com>
2023-12-16 20:14:45 +0600

    Rewrite to ES6

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-function startGame() {
+const startGame = () => {
   myGamePiece = new component(30, 30, "red", 10, 120);
   myGamePiece.gravity = 0.05;
   myScore = new component("30px", "Consolas", "black", 280, 40, "text");
   myGameArea.start();
 }
 
-var myGameArea = {
+const myGameArea = {
   canvas : document.createElement("canvas"),
-  start : function() {
+  start : () => {
     this.canvas.width = 480;
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
   },
-  clear : function() {
+  clear : () => {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
